b43986c9e2fd3938988354931a40d8c0cd62cf86
yuanzepingyzp <2808551527@qq.com>
2017-01-10 11:26:47 +0800

    Create yzpLoading.js

---
+++ yzpLoading.js
@@ -0,0 +1,58 @@
+var yzpLoad=angular.module("app",[]);
+yzpLoad.directive("yzpLoad",function($timeout){
+	return{
+		scope:{
+			speed:'@',
+			yzpId:'@'
+		},
+		restrict:'E',
+		template:'<canvas id={{yzpId}}></canvas>',
+		replace:true,
+		link:function(scope){
+			$timeout(function(){new yzpLoading(scope.yzpId,scope.speed);},100);
+		}
+	}
+})
+function yzpLoading(id,speed){
+	var canvas,context;
+
+	this.width=100;
+	this.height=100;
+	this.color="rgb(100,200,200)";
+	this.id=id;
+	this.angle=0;
+	this.r=this.width/4;
+	this.speed=speed;
+	this.clear=function(){
+		context.beginPath();
+		context.clearRect(this.width/2-this.r-5,this.height/2-this.r-5,2*this.r+10,2*this.r+10)
+	}
+	this.render=function(){
+		context.beginPath();
+		context.arc(this.width/2,this.height/2,this.r,0,this.angle);
+		context.lineWidth=5;
+		context.strokeStyle=this.color;
+		context.stroke();
+	};
+	this.loading=function(){
+		this.clear();
+		var This=this;
+		if(this.angle<2*Math.PI){
+			this.angle=this.angle+Math.PI/180*this.speed;
+		}else{
+			this.angle=0;
+		}
+		
+		this.render();
+		loadAnimation=window.requestAnimationFrame(function(){This.loading()});
+	};
+	this.init=(function(This){
+		canvas=document.getElementById(This.id);
+		context=canvas.getContext("2d");
+		canvas.width=This.width;
+		canvas.height=This.height;
+		canvas.style.backgroundColor="rgb(100,100,100)";
+		canvas.style.borderRadius="5px";
+		This.loading();
+	})(this);
+}
